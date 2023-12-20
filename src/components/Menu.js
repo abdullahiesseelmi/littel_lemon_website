95eafef482c1ba2db64f34fb6fb7c7ddf804d6f7
mohamed daahir faarah <mohamed.dahir@tabaarakict.org>
2023-12-20 09:08:04 +0300

    i added sweetalert2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,29 @@
 import React from "react";
 import recipes from "../recipes";
-
+import Swal from "sweetalert2";
 const Menu = () => {
+
+  const handleOrder = (id) => {
+    //console.log("handleOrder is " + id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success"
+        });
+      }
+    });
+  }
+
   return (
     <div className="menu-container">
       <div className="menu-header">
@@ -19,7 +41,7 @@ const Menu = () => {
                 <p>${recipe.price}</p>
               </div>
               <p>{recipe.description}</p>
-              <button className="orderBtn">Order Now</button>
+              <button className="orderBtn" onClick={() => handleOrder(recipe.id)}>Order Now</button>
             </div>
           </div>
         ))}
